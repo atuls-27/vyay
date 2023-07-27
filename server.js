bf34ad243052dc66701c2678a1d60350a864ab20
atuls-27 <129782454+atuls-27@users.noreply.github.com>
2023-07-27 23:50:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDb = require("./config/connectDB");
+
+// config dot env file
+dotenv.config();
+
+//databse call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use("/api/v1/users" , require ("./routes/userRoute" ));
+
+//transections routes
+app.use("/api/v1/transactions", require("./routes/transactionRoute"));
+
+//port
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
